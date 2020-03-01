35bbb19c4a6d371dfa4063ca325361c0a00c8c64
ditafazri <ochasfaz@gmail.com>
2020-03-01 14:15:27 +0700

    tampilkan cuaca list html

--- application/controllers/Dita.php
+++ application/controllers/Dita.php
@@ -3,8 +3,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Dita extends CI_Controller {
     public function index()
-    {
-        $data = [];
+        
+        {
+$api = new RestClient([
+	'base_url' => "https://ibnux.github.io/BMKG-importer",
+	'format' => "json"
+    ]);
+$result = $api->get("cuaca/501320");
+$data['data']= $result->decode_response();
         $data['name'] = "Dita Fazri Chairunnisa";
         $this->load->view('dita', $data);
     }
--- application/views/dita.php
+++ application/views/dita.php
@@ -6,6 +6,20 @@
 <body>
 
 <h2> Nama Lengkap : <?php echo $name ?></h2>
-
+ <h3>Ramalan Cuaca : </h3>
+    <ul>
+    <?php foreach((array) $data as $item) { ?>
+	
+    <?php $date = Carbon\Carbon::createFromFormat('Y-m-d H:i:s', @$item-> jamCuaca);?>
+        <li>
+        Cuaca kota Singkawang hari <?php echo $date->format('l'); ?>
+        tanggal <?php echo $date->format('d M Y'); ?>
+        jam <?php echo $date->format('H:i'); ?>
+        adalah <b> <?php echo $item->cuaca; ?></b>
+        dengan suhu <?php echo $item->tempC; ?>
+        derajat celcius
+        </li>
+        <?php } ?>
+   </ul>
 </body>
 </html>
